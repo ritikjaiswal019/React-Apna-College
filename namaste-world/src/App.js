8afdffa891cd8fc8213b66138ce15fd8f6c3c88a
ritik <ritikjaiswal019@gmail.com>
2022-05-25 19:26:59 +0530

    First Sitting

---
+++ namaste-world/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+    <p>{1+5*5}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Namaste World
+        </a>
+      </header>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
